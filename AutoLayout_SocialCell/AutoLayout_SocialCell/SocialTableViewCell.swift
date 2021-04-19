81dd346add0a63e9ae41f1a3cfd4d5d3a2e144d1
jsjphone8 <jsjphone8@gmail.com>
2021-04-19 23:20:41 +0900

    Feed 데이터 적용, 오토레이아웃 개발 완료 근데 여전히 충돌이 난다고 뜨는 부분은 괜찮은지 의문임

--- AutoLayout_SocialCell/AutoLayout_SocialCell/SocialTableViewCell.swift
+++ AutoLayout_SocialCell/AutoLayout_SocialCell/SocialTableViewCell.swift
@@ -8,66 +8,126 @@
 import UIKit
 
 class SocialTableViewCell: UITableViewCell {
-    private var stackView: UIStackView!
-    var profileImageView: UIImageView!
-    var nameLabel: UILabel!
-    var uploadTimeLabel: UILabel!
-    var bodyTextLabel: UILabel!
-    var bodyImageView: UIImageView!
-    var likeCount: UILabel!
-    private var thumbsUpImageView: UIImageView!
-    private var buttonStackView: UIStackView!
+    
+    var feed: Feed! {
+        didSet {
+            profileImageView?.image = feed.author.profileImage
+            nameLabel?.text = feed.author.name
+            uploadTimeLabel?.text = feed.time
+            bodyTextLabel?.text = feed.contents.text
+            bodyImageView?.image = feed.contents.image
+            likeCountLabel?.text = "\(feed.likes)"
+            
+            bodyTextLabel?.isHidden = bodyTextLabel?.text?.isEmpty == true
+            bodyImageView?.isHidden = bodyImageView?.image == nil
+            
+        }
+    }
+    
+    private var profileImageView: UIImageView!
+    private var nameLabel: UILabel!
+    private var uploadTimeLabel: UILabel!
+    private var bodyTextLabel: UILabel!
+    private var bodyImageView: UIImageView!
+    private var likeCountLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
 
-        setUp()
+        setUI()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style,
+                   reuseIdentifier: reuseIdentifier)
+        
+        setUI()
     }
-
-    private func setUp() {
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+ 
+    private func setUI() {
+        selectionStyle = .none
+        
         // profile line
         profileImageView = UIImageView()
         profileImageView.translatesAutoresizingMaskIntoConstraints = false
-//        profileImageView.setContentHuggingPriority(.defaultHigh, for: .horizontal) // 얘 다시 확인해보기
+        profileImageView.clipsToBounds = true
+        profileImageView.contentMode = .scaleAspectFit
          
         nameLabel = UILabel()
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.font = .preferredFont(forTextStyle: .caption1)
-//        nameLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        nameLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
+        nameLabel.setContentCompressionResistancePriority(.required, for: .vertical) // 중요한 내용이니 보존하는 역할로
         
         uploadTimeLabel = UILabel()
-        uploadTimeLabel.translatesAutoresizingMaskIntoConstraints = false
         uploadTimeLabel.font = .preferredFont(forTextStyle: .caption2)
         uploadTimeLabel.setContentHuggingPriority(.init(1), for: .horizontal)
         uploadTimeLabel.textAlignment = .left
         
         let profileStack = UIStackView(arrangedSubviews: [profileImageView, nameLabel, uploadTimeLabel])
-        profileStack.translatesAutoresizingMaskIntoConstraints = false
         profileStack.axis = .horizontal
-        profileStack.distribution = .fill
         profileStack.alignment = .center
         profileStack.spacing = 5
         
         // body
         bodyTextLabel = UILabel()
-        bodyTextLabel.translatesAutoresizingMaskIntoConstraints = false
         bodyTextLabel.font = .preferredFont(forTextStyle: .body)
         bodyTextLabel.numberOfLines = 0
         bodyTextLabel.setContentCompressionResistancePriority(.required, for: .vertical)
         
         bodyImageView = UIImageView()
-        bodyImageView.translatesAutoresizingMaskIntoConstraints = false
         bodyImageView.clipsToBounds = true
         bodyImageView.contentMode = .scaleAspectFill
+        bodyImageView.isUserInteractionEnabled = true
+        
+        // footer
+        let thumbsUpImageView: UIImageView = { imageView in
+            imageView.image = UIImage(systemName: "hand.thumbsup.fill")
+            
+            return imageView
+        } (UIImageView())
+        
+        likeCountLabel = { label in
+            label.font = .preferredFont(forTextStyle: .callout)
+            label.setContentHuggingPriority(.defaultLow, for: .horizontal)
+            label.text = "0개"
+            
+            return label
+        } (UILabel())
+        
+        let likeStackView: UIStackView = { stack in
+            stack.axis = .horizontal
+            stack.distribution = .fill
+            stack.alignment = .center
+            stack.spacing = UIStackView.spacingUseSystem
+            
+            return stack
+        } (UIStackView(arrangedSubviews: [thumbsUpImageView, likeCountLabel]))
+        
+        let buttonStackView: UIStackView = { stack in
+            stack.axis = .horizontal
+            stack.distribution = .fillEqually
+            stack.alignment = .fill
+            
+            let buttonTitles = ["좋아요", "댓글 달기", "공유하기"]
+            buttonTitles.forEach { title in
+                let button = UIButton(type: .system)
+                button.setTitle(title, for: .normal)
+                button.setTitleColor(.darkGray, for: .normal)
+                button.layer.borderColor = UIColor.darkGray.cgColor
+                button.layer.borderWidth = 1
+                
+                stack.addArrangedSubview(button)
+            }
+            
+            return stack
+        } (UIStackView())
         
         // Whole Stack View
-        stackView = UIStackView(arrangedSubviews: [profileStack, bodyTextLabel, bodyImageView])
+        let stackView = UIStackView(arrangedSubviews: [profileStack, bodyTextLabel, bodyImageView, likeStackView, buttonStackView])
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.distribution = .fill
@@ -83,8 +143,13 @@ class SocialTableViewCell: UITableViewCell {
             stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
             profileImageView.widthAnchor.constraint(equalTo: profileImageView.heightAnchor),
             profileImageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.1),
-            bodyImageView.widthAnchor.constraint(equalTo: bodyImageView.heightAnchor)
+            bodyImageView.widthAnchor.constraint(equalTo: bodyImageView.heightAnchor),
         ])
         
+        thumbsUpImageView.widthAnchor.constraint(equalTo: thumbsUpImageView.heightAnchor).isActive = true
+        thumbsUpImageView.heightAnchor.constraint(greaterThanOrEqualToConstant: 30).isActive = true
+        let thumbsUpHeight = thumbsUpImageView.heightAnchor.constraint(lessThanOrEqualTo: likeCountLabel.heightAnchor)
+        thumbsUpHeight.priority = .defaultHigh
+        thumbsUpHeight.isActive = true
     }
 }
--- AutoLayout_SocialCell/AutoLayout_SocialCell/ViewController.swift
+++ AutoLayout_SocialCell/AutoLayout_SocialCell/ViewController.swift
@@ -13,36 +13,40 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configureTable()
     }
 
-
+    private func configureTable() {
+        self.tableView.register(SocialTableViewCell.self, forCellReuseIdentifier: "cell")
+    }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        20
+        100
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        guard let socialCell = cell as? SocialTableViewCell else {
-            return cell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? SocialTableViewCell else {
+            return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         }
         
-        socialCell.profileImageView.image = UIImage(systemName: "person")
-        socialCell.nameLabel.text = "정성훈"
-        socialCell.uploadTimeLabel.text = "20시 30분"
-        socialCell.bodyTextLabel.text = "짧은 글토막"
+//        socialCell.profileImageView.image = UIImage(systemName: "person")
+//        socialCell.nameLabel.text = "정성훈"
+//        socialCell.uploadTimeLabel.text = "20시 30분"
+////        socialCell.bodyTextLabel.text = "짧은 글토막"
 //        socialCell.bodyTextLabel.text = """
 //            이것은 긴 텍스트를 아무렇게나 적고 있는 건데 어디까지 호루루루루루루루ㅜ루룰
 //            적힐지는 모르겠고 아무튼 트위터처럼 적는거고 나도 내가 새벽 한시에 호로로ㅗ겨롣개ㅕㅙ
 //            이걸 왜 하고 있는지도 모르겠지만
 //            그래도 재밌으니 합니다
 //            """
-        socialCell.bodyImageView.image = UIImage(named: "\(indexPath.row % 3)")
+//        socialCell.bodyImageView.image = UIImage(named: "\(indexPath.row % 3)")
+//
         
-        return socialCell
+//        cell.feed = Feed.feeds[Int.random(in: 0...9)]
+        cell.feed = Feed.feeds[indexPath.row % 10]
+        return cell
     }
     
     

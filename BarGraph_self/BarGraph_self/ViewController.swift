9f0674e70fa459dc5569e4b9d01edbed7c6a0e0e
jsjphone8 <jsjphone8@gmail.com>
2021-04-22 12:11:20 +0900

    바그래프 StackView간 constraint 적용

---
+++ BarGraph_self/BarGraph_self/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BarGraph_self
+//
+//  Created by 정성훈 on 2021/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
